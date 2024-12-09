5ea95d007c096836943ac0e15931551fab22eba9
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-12-09 14:07:04 -0500

    Delete src/models/User.ts

--- src/models/User.ts
+++
@@ -1,9 +0,0 @@
-import { model, Schema } from "mongoose";
-
-const schema = new Schema({
-    _id: String,
-    credit_amount: Number,
-    credit_used: Number
-})
-
-export default model("users", schema, "users");
