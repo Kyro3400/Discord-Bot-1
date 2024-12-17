4ed79c590beebaf1c29edb9f1e1796ac60b0f5a0
Curry <noobinthehouse345@gmail.com>
2024-12-17 15:40:03 +0000

    Update serverSpecs.ts
    Added GPU to the specs, and added dono-5 specs

--- src/keywords/serverSpecs.ts
+++ src/keywords/serverSpecs.ts
@@ -10,13 +10,13 @@ const keyword: Keyword = {
     \n- **Pnode-3:** \`N/A\`
     \n\n**__Premium Nodes__**
     \n__Node | CPU | RAM | SSD | Network__
-    \n- **Dono-1:** \`32 Cores, 64 Threads | 128GB | 2TB | 1Gbps up/down\`
-    \n- **Dono-2:** \`6 Cores, 12 Threads  | 128GB | 2TB | 1Gbps up/down\`
-    \n- **Dono-3:** \`6 Cores, 12 Threads  | 128GB | 2TB | 1Gbps up/down\`
-    \n- **Dono-4:** \`6 Cores, 12 Threads  | 128GB | 2TB | 1Gbps up/down\`
-    \n- **Dono-5:** \`N/A\`
+    \n- **Dono-1:** \`32 Cores, 64 Threads | 128GB | 2TB | 1Gbps up/down | GPU: No\`
+    \n- **Dono-2:** \`6 Cores, 12 Threads  | 128GB | 2TB | 1Gbps up/down | GPU: No\`
+    \n- **Dono-3:** \`6 Cores, 12 Threads  | 128GB | 2TB | 1Gbps up/down | GPU: No\`
+    \n- **Dono-4:** \`6 Cores, 12 Threads  | 128GB | 2TB | 1Gbps up/down | GPU: No\`
+    \n- **Dono-5:** \`12 Cores, 24 Threads | 128GB | 2TB | 1Gbps up/down | GPU: Yes\`
     \n\n**__VPS Nodes__**
-    \n- **US-1:** \`16 Cores, 32 Threads   | 128GB | 2TB | 1Gbps up/down\`
+    \n- **US-1:** \`16 Cores, 32 Threads   | 128GB| 2TB | 1Gbps up/down\`
     \n- **US-2:** \`16 Cores, 32 Threads   | 96GB | 1TB | 2.5Gbps up/down\`
     \n- **UK-1:** \`12 Cores\ 24 Threads   | 64GB | 2TB | 1Gbps up/down\``,
     minimumKeywords: 3,
