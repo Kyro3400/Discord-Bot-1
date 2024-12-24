6b5c4e7e8382c9d7aa6e18c355ad356c49d53b94
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-12-23 20:55:56 -0500

    chore: formatting

--- src/keywords/vpsSpecs.ts
+++ src/keywords/vpsSpecs.ts
@@ -5,14 +5,15 @@ const keyword: Keyword = {
     keywords: ["price", "vps"],
     requiredKeywords: ["vps"],
     response: `Pricing is as follows:
-        -# Please note, pricing is monthly.
         $1 - 1 vCPU, 1GB RAM, 100GB disk
         $2 - 1 vCPU, 2GB RAM, 100GB disk
         $3 - 2 vCPUs, 3GB RAM, 150GB disk
         $4 - 2 vCPUs, 4GB RAM, 150GB disk
         $5 - 3 vCPUs, 5GB RAM, 200GB disk
         $6 - 3 vCPUs, 6GB RAM, 200GB disk
-        $7 - 4 vCPUs, 7GB RAM, 250GB disk`,
+        $7 - 4 vCPUs, 7GB RAM, 250GB disk
+        
+        -# Please note, pricing is monthly.`,
     minimumKeywords: 1,
     matchAll: false,
     enabled: true
