70ebf7e86380a39df14ca16a660f305a9810ce22
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2026-01-17 13:23:31 -0500

    chore: update some FAQs

--- src/keywords/chargeBack.ts
+++ src/keywords/chargeBack.ts
@@ -5,7 +5,8 @@ const keyword: Keyword = {
     keywords: ["chargeback", "paypal", "bank"],
     requiredKeywords: ["chargeback"],
     response: `As this has always been the case since we started offering a donation link, it will result in a instant ban. \n
-    As per our ToS you can request a refund with no questions asked within 14 days of the donation. After that if you are not happy with the services offered you can open a ticket and we will try our best to fix any issues that you have encountered.`,
+    As per our ToS you can request a refund with no questions asked within 14 days of the donation. After that if you are not happy with the services offered you can open a ticket and we will try our best to fix any issues that you have encountered.\n
+    See our [Terms of Service](https://danbot.host/tos) and [Chargeback Policy](https://docs.danbot.host/policies/chargeback) respectively.`,
     minimumKeywords: 1,
     matchAll: false,
     enabled: true
